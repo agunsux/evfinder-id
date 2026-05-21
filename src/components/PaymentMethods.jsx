b64f96949fba8f3debbc708a45927d1f766f94a9
Baginda Agun <agunsux@users.noreply.github.com>
2026-05-22 01:51:27 +0700

    feat: update logos for payment methods and main logo

--- src/components/PaymentMethods.jsx
+++ src/components/PaymentMethods.jsx
@@ -11,14 +11,15 @@ const PaymentMethods = () => {
         <img src="/payment/dana.png" alt="DANA" className="h-8" />
         <img src="/payment/gopay.png" alt="GoPay" className="h-8" />
         <img src="/payment/ovo.png" alt="OVO" className="h-8" />
-        <img src="/payment/shopeepay.png" alt="ShopeePay" className="h-8" />
-        <img src="/payment/qris.png" alt="QRIS" className="h-10" /> 
+        <img src="/payment/shopee.png" alt="ShopeePay" className="h-8" />
+        <img src="/payment/LinkAja.png" alt="LinkAja" className="h-8" />
+        <img src="/payment/QRIS.png" alt="QRIS" className="h-10" /> 
       </div>
       <div className="flex flex-wrap justify-center items-center gap-6">
-        <span className="text-sm font-bold text-text-muted">Virtual Account:</span>
+        <img src="/payment/virtual account.png" alt="Virtual Account" className="h-6" />
         <span className="text-sm font-bold text-text">BCA, Mandiri, BNI, BRI</span>
         <img src="/payment/mastercard.png" alt="MasterCard" className="h-6" />
-        <img src="/payment/visa.png" alt="Visa" className="h-6" />
+        <img src="/payment/Visa.png" alt="Visa" className="h-6" />
         <img src="/payment/paypal.png" alt="PayPal" className="h-6" />
       </div>
     </div>
--- src/components/ShinervaLogo.jsx
+++ src/components/ShinervaLogo.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
 const ShinervaLogo = ({ className }) => (
-  <svg className={className} viewBox="0 0 100 100" fill="none" stroke="currentColor" strokeWidth="6" strokeLinecap="round">
-    <path d="M50 85c-15 0-25-10-25-25 0-15 10-25 25-25 15 0 25 10 25 25C75 75 65 85 50 85z" />
-    <path d="M50 70c-8 0-12-6-12-15 0-9 4-15 12-15 8 0 12 6 12 15C62 64 58 70 50 70z" />
-    <path d="M50 95C25 95 5 75 5 50 5 25 25 5 50 5 75 5 95 25 95 50 95 75 75 95 50 95z" />
-    <path d="M50 85C30 85 15 70 15 50 15 30 30 15 50 15 70 15 85 30 85 50 85 70 70 85 50 85z" />
-    <path d="M50 75C35 75 25 65 25 50 25 35 35 25 50 25 65 25 75 35 75 50 75 65 65 75 50 75z" />
-    <path d="M50 65C40 65 35 60 35 50 35 40 40 35 50 35 60 35 65 40 65 50 65 60 60 65 50 65z" />
-  </svg>
+  <img src="/logo.png" alt="Shinerva Logo" className={className} />
 );
 
 export default ShinervaLogo;
